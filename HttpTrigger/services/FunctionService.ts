25fe01ae77bbda1e22a27663bc50d573856ace7a
Filip Szot <szot@Filips-MacBook-Pro.local>
2021-08-17 22:32:13 +0200

    working api

--- HttpTrigger/services/FunctionService.ts
+++ HttpTrigger/services/FunctionService.ts
@@ -2,8 +2,6 @@ import { inject, injectable } from "inversify";
 import { IFunctionService } from "./IFunctionService";
 import { COMMON_TYPES } from "../../ioc/commonTypes";
 import { ILogger } from "../../commonServices/iLogger";
-import axios from "axios";
-import { Pokemon } from "../../Pokemon/pokemon";
 import { PokemonService } from "../../Pokemon/PokemonService";
 
 @injectable()
@@ -11,30 +9,16 @@ export class FunctionService implements IFunctionService<any> {
 
     @inject(COMMON_TYPES.ILogger)
     private readonly _logger: ILogger;
-    // const container: Container = getContainer();
-    // const pokemonService: PokemonService<any> =
-    //     container.get<PokemonService<any>>(COMMON_TYPES.PokemonService);
 
     public async processMessageAsync(msg: any): Promise<any> {
         this._logger.info("Hello world");
         this._logger.verbose(`${JSON.stringify(msg)}`);
         return {msg: "success"};
     }
-    public async getPokemons(query: {}): Promise<any> {
-        console.log(query);
-        let key: string = null;
-        let value: string;
-        let ids: [] = [];
+    public async getPokemons(query: {}): Promise<any> {        
+        return PokemonService.loadPokemons(query).then( (res) => {
+            return res;
+        });
 
-        for ( const k in query ) {
-            if ( k !== "id" && key === null ) {
-                key = k;
-                value = query[k];
-            } else if ( k === "id" ) {
-                ids = query[k];
-            }
-        }
-
-        return PokemonService.getPokemonsByParams(key, value);
     }
 }
--- Pokemon/PokemonService.ts
+++ Pokemon/PokemonService.ts
@@ -1,21 +1,84 @@
 import { injectable } from "inversify";
 import axios from "axios";
 import { Pokemon } from "./pokemon";
+import _ from "lodash";
 
 @injectable()
 export class PokemonService {
-    public static async getPokemonsByParams(param: string, value: string): Promise<Pokemon[]> {
-        let response: any = [];
-        const url: string = `https://pokeapi.co/api/v2/${param}/${value}`;
-        const data: any = await axios.get(url);
-        if (data.hasOwnProperty("data") && data.data.hasOwnProperty("pokemon")) {
-            response = data.data.pokemon;
+    public static async loadPokemons(params: any): Promise<Pokemon[]> {
+        const promises: any[] = [];
+        for ( const key in params ) {
+            if ( key !== "id" ) {
+                promises.push(this.getPokemonsByParams(key, params[key]));
+            } else if ( key === "id" ) {
+                const ids: string = params[key];
+                ids.split(",").forEach((element) => {
+                    promises.push(this.getPokemonById(element));
+                });
+            }
         }
-        const array: Pokemon[] = response.map(
-            (obj) => {
-            return new Pokemon(obj.pokemon.url, obj.pokemon.name);
+        return Promise.all(promises).then( (res) => {
+            return this.intersectionElements(res);
         });
+    }
+    
+    public static async getPokemonsByParams(param: string, value: string): Promise<Pokemon[]> {
+        let response: any[] = [];
+        const url: string = `https://pokeapi.co/api/v2/${param}/${value}`;
+        try {
+            const data: any = await axios.get(url);
+            if (data.hasOwnProperty("data") && data.data.hasOwnProperty("pokemon")) {
+                response = data.data.pokemon;
+            }
+            return response.map(
+                (obj) => {
+                    if ( obj.hasOwnProperty("pokemon")) {
+                        return new Pokemon(obj.pokemon.url, obj.pokemon.name);
+                    }
+            });
 
-        return array;
+        } catch (error) {
+            console.log(error.response.data.error);
+        }
+    }
+
+    public static async getPokemonById(id: string): Promise<Pokemon> {
+        let response: Pokemon;
+        const url: string = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        try {
+            const data: any = await axios.get(url);
+            if (data.hasOwnProperty("data")) {
+                response = data.data;
+                if (response.hasOwnProperty("name")) {
+                    return new Pokemon(url, response.name);
+                } else {
+                    return null;
+                }
+            }
+        } catch (error) {
+            console.log(error.response.data.error);
+        }
+    }
+
+    private static intersectionElements(array: any[]): Pokemon[] {
+        let result: Pokemon[] = [];
+        const elementsById: Pokemon[] = [];
+        array.forEach( (element) => {
+            if (element !== null) {
+                if (!Array.isArray(element)) {
+                    elementsById.push(element);
+                } else {
+                    if (result.length < 1) {
+                        result = _.union(result, element);
+                    } else {
+                        result = _.intersectionBy(result, element, "id");
+                    }
+                }
+            }
+        });
+        if (elementsById.length > 0) {
+            result = _.intersectionBy(result, elementsById, "id");
+        }
+        return result;
     }
 }
